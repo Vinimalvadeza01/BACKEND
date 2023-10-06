4a982072e44806cd2698c3bc88d6ebad302d61a3
guizult <lhermesou33@gmail.com>
2023-10-06 11:54:04 -0300

    subindo

--- Guilherme-Almeida/API/src/index.js
+++ Guilherme-Almeida/API/src/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config'
-import {con } from './repository/connection.js'
 import express from 'express'
 import cors from 'cors'
+
+import usuarioController from './controller/usuarioController.js';
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.use(usuarioController);
 
 server.listen(process.env.PORT,
     () => console.log(`API subiuu ${process.env.PORT}`))
--- Guilherme-Almeida/API/src/repository/usuarioRepository.js
+++ Guilherme-Almeida/API/src/repository/usuarioRepository.js
@@ -1,15 +1,14 @@
 import {con} from './connection.js'
 
-export async function login(email, senha) {
+export async function login(emailCPF ) {
     const comando = `Select         
         DS_EMAIL    as Email,
         DS_CPF        as CPF,
         DS_SENHA     as Senha
         from tb_cliente
-        Where DS_EMAIL=?  OR DS_CPF=?
-        AND DS_SENHA=?`
-        
-    const [linhas] = await con.query(comando,  [email, senha])
-    return linhas[0];  
+        Where DS_EMAIL=?  OR DS_CPF=?`
+            
+    const [linhas] = await con.query(comando,  [emailCPF, emailCPF]);
 
+    return linhas;  
 }
