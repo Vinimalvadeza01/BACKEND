6362e152fde8d2e1c693c8fed259061ae254bc5d
VInicius <Vvini3881@gmail.com>
2023-10-09 11:53:09 -0300

    API DE CADASTRO COMPLETA

--- Banco de Dados/comandoscadastros/script.sql
+++ Banco de Dados/comandoscadastros/script.sql
@@ -1,2 +1,17 @@
+
+
+##inserir os dados pedidos na pagina de cadastro##
+
   INSERT INTO tb_cliente (nm_nome, ds_email, nr_cpf, dt_datanasc, ds_senha)
 		VALUE (?,?,?,?,?);
+
+
+##selects para a verificação dos dados e-mail e cpf para ver se os dados ja existem##
+
+      SELECT id_cliente    as id,
+          ds_cpf       as cpf
+  FROM tb_cliente WHERE ds_cpf=?;
+
+    SELECT id_cliente    as id,
+          ds_email       as email
+  FROM tb_cliente WHERE ds_email=?;
